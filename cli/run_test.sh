02b4e05a042dedfd973a1642995a605b0ab50b21
Andrey Kravtsun <kravtsun@cvisionlab.com>
2017-03-03 13:39:11 +0300

    Test environment changes. removed test_runner.cpp from repository as in the same time run_test.sh shows how to generate it and run all the tests.

--- cli/run_test.sh
+++ cli/run_test.sh
@@ -1,27 +1,15 @@
-#!/bin/sh
-# 
-# rm -f src/cxxtest_runner.cpp
-# 
-# make
-# 
-# rm -f main.o
-# 
-# OBJECTS=$(ls *.o)
-# 
-# HEADERS_FILE=cxxtest-headers
-# 
-# rm -f ${HEADERS_FILE}
-# 
-# ls -1 *Test.h > ${HEADERS_FILE}
-# 
-# ./cxxtest/bin/cxxtestgen --have-std --runner=ErrorPrinter --headers=${HEADERS_FILE} -o runner.cpp
-# 
-# clang -I./cxxtest -std=c++11 -lstdc++ -o testrunner runner.cpp $OBJECTS
-# 
-# ./testrunner $@
-# 
-# rm -f runner.cpp ${HEADERS_FILE}
+#!/bin/bash
+set -e
+qmake -o bin/Makefile
+cd bin
+TEST_HEADERS_FILE=cxxtest-headers
+ls -1 ../include/*_test.h > ${TEST_HEADERS_FILE}
 
-g++ -std=c++11 -I third_party/cxxtest -I include/ src/test_runner.cpp -o bin/test_runner.out
-third_party/cxxtest/bin/cxxtestgen --have-std --runner=ErrorPrinter -o cli_tester/test_runner.cpp include/cli_cat_command_test.h  include/cli_command_parser_test.h  include/cli_command_queue_test.h  include/cli_environment_test.h
+../third_party/cxxtest/bin/cxxtestgen --have-std --runner=ErrorPrinter -o test_runner.cpp --headers=${TEST_HEADERS_FILE}
+
+make
+OBJECTS=$(ls *.o | grep -v main.o)
+g++ -std=c++11 -I ../third_party/cxxtest -I ../include -o test_runner.out test_runner.cpp ${OBJECTS}
+
+./test_runner.out
  
