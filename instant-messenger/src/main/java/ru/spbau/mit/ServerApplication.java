ae516a160fccb17c0262ea875a3bcb7f825e711f
Andrey Kravtsun <avkravtsun@gmail.com>
2017-06-08 16:59:09 +0300

    Peer-to-peer communications.

--- instant-messenger/src/main/java/ru/spbau/mit/ServerApplication.java
+++ instant-messenger/src/main/java/ru/spbau/mit/ServerApplication.java
@@ -1,23 +1,31 @@
 package ru.spbau.mit;
 
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
+import io.grpc.stub.StreamObserver;
 import org.apache.commons.cli.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
+import java.io.Reader;
+import java.util.Objects;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class ServerApplication {
     private static final Logger logger = LogManager.getLogger("server");
     private Server server;
+    private final String name;
 
     /**
      * @param host not used now.
      * @param port serving port.
      * @throws IOException on failing to start and bind to server.
      */
-    private ServerApplication(String host, int port) throws IOException {
+    private ServerApplication(String host, int port, String name) throws IOException {
         // TODO use host for creating service daemon.
         server = ServerBuilder.forPort(port)
                 .addService(new SimpleMessageService())
@@ -30,6 +38,7 @@ public class ServerApplication {
             ServerApplication.this.stop();
             System.err.println("*** server shut down");
         }));
+        this.name = name;
     }
 
     private void stop() {
@@ -38,18 +47,89 @@ public class ServerApplication {
         }
     }
 
-    /**
-     * Await termination on the main thread since the grpc library uses daemon threads.
-     */
-    private void blockUntilShutdown() throws InterruptedException {
-        if (server != null) {
-            server.awaitTermination();
+    private class Connection {
+        private ManagedChannel channel;
+        private StreamObserver<Message> requestObserver;
+
+        Connection(String host, int port) {
+            logger.debug("Setting ManagedChannelBuilder...");
+            channel = ManagedChannelBuilder.forAddress(host, port)
+                    .usePlaintext(true)
+                    .build();
+
+            logger.debug("Setting MessageServiceStub...");
+            MessageServiceGrpc.MessageServiceStub stub = MessageServiceGrpc.newStub(channel);
+
+            StreamObserver<Message> responseObserver = new StreamObserver<Message>() {
+                @Override
+                public void onNext(Message value) {
+                    logger.info("received message: \n" + value);
+//                    String receiver = value.getReceiver();
+//                    if (!receiver.equals(name)) {
+//                        String errorMessage = "Invalid receiver: " + receiver + " for name: " + name;
+//                        logger.error(errorMessage);
+//                        throw new IllegalStateException(errorMessage);
+//                    }
+                    System.out.println(value.getSender() + " wrote: " + value.getText());
+                }
+
+                @Override
+                public void onError(Throwable t) {
+                    logger.error(t.getMessage());
+                    System.out.println("ERROR: " + t.getMessage());
+//                    shutdown(1);
+                }
+
+                @Override
+                public void onCompleted() {
+                    logger.info("onCompleted, name: " + name);
+//                    shutdown(0);
+                }
+            };
+
+            logger.debug("Setting chat connection...");
+            requestObserver = stub.chat(responseObserver);
+        }
+
+        public void send(String text) {
+            logger.info("sending message: \n" + text);
+            Message message = Message.newBuilder()
+                    .setText(text)
+                    .setTime(SimpleMessageService.currentDateString())
+                    .setSender(name)
+                    .build();
+            requestObserver.onNext(message);
+            logger.info("message sent: \n" + message);
+
+        }
+
+        public void shutdown() {
+            try {
+                channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+            } catch (InterruptedException e) {
+                logger.error(e.getMessage());
+            }
         }
     }
+
+
+    private Connection getConnection(String receiverHost, int receiverPort) {
+        return new Connection(receiverHost, receiverPort);
+    }
+
+//    /**
+//     * Await termination on the main thread since the grpc library uses daemon threads.
+//     */
+//    private void blockUntilShutdown() throws InterruptedException {
+//        if (server != null) {
+//            server.awaitTermination();
+//        }
+//    }
 
     public static void main(String[] args) {
         String hostOptionString = "host";
         String portOptionString = "port";
+        String nameOptionString = "name";
 
         Options options = new Options();
 
@@ -71,9 +151,20 @@ public class ServerApplication {
                 .desc("Serving port.")
                 .build();
 
+        Option nameOption = Option.builder()
+                .argName(nameOptionString)
+                .longOpt(nameOptionString)
+                .hasArg(true)
+                .type(String.class)
+                .required(true)
+                .desc("name")
+                .build();
+
         options.addOption(hostOption);
         options.addOption(portOption);
-        assert options.getRequiredOptions().size() == 2;
+        options.addOption(nameOption);
+
+        assert options.getRequiredOptions().size() == 3;
 
         CommandLineParser parser = new DefaultParser();
         HelpFormatter formatter = new HelpFormatter();
@@ -85,29 +176,68 @@ public class ServerApplication {
             logger.error("Error while trying to parse options: " + e.getMessage());
             System.err.println(e.getMessage());
             // TODO make help message display required options in USAGE string.
-            formatter.printHelp("instant-messenger-server", options);
+            formatter.printHelp("instant-messenger", options);
             System.exit(1);
         }
 
         // TODO use options to get parsed values.
         String host = cmd.getOptionValue(hostOptionString);
-        int port = Integer.valueOf (cmd.getOptionValue(portOptionString));
-        logger.debug("Parsed options: " + "host: " + host + ", port: " + port);
+        int port = Integer.valueOf(cmd.getOptionValue(portOptionString));
+        final String name = cmd.getOptionValue(nameOptionString);
+        logger.debug("Parsed options: "
+                + "host: " + host
+                + ", port: " + port
+                + ", name: " + name);
         ServerApplication serverApplication = null;
         try {
-            serverApplication = new ServerApplication(host, port);
+            serverApplication = new ServerApplication(host, port, name);
         } catch (IOException e) {
             logger.error("Error while trying to create server application: " + e.getMessage());
             System.err.println(e.getMessage());
             System.exit(1);
         }
 
-        logger.debug("Server start successful, hanging up");
-        try {
-            serverApplication.blockUntilShutdown();
-        } catch (InterruptedException e) {
-            logger.info("interrupt signal received: " + e.getMessage());
+        String messageFormatHelp =
+                "connect <host> <port>\n" +
+                        "disconnect\n" +
+                        "exit\n";
+//                "Message format: <message-receiver-name>: message-text\n" +
+//                "where <message-receiver-name> is non-empty sequence of colon-less symbols\n" +
+//                "submit \"exit\" to exit.";
+        System.out.println(messageFormatHelp);
+
+        Scanner stdin = new Scanner(System.in);
+        Connection connection = null;
+        while (true) {
+            String line = stdin.nextLine();
+            try {
+                if (line.equals("exit")) {
+                    break;
+                } else if (!Objects.isNull(connection)) {
+                    if (line.equals("disconnect")) {
+                        connection.shutdown();
+                        connection = null;
+                    } else {
+                        connection.send(line);
+                    }
+                } else {
+                    if (line.length() > 7 && line.substring(0, 7).equals("connect")) {
+                        Scanner in = new Scanner(line);
+                        String s = in.next("connect");
+                        if (!s.equals("connect")) {
+                            throw new IllegalStateException("Command parsing connect command");
+                        }
+                        String receiverHost = in.next();
+                        int receiverPort = in.nextInt();
+                        connection = serverApplication.getConnection(receiverHost, receiverPort);
+                    } else {
+                        throw new IllegalArgumentException("Illegal command: " + line);
+                    }
+                }
+            } catch (Exception e) {
+                logger.error(e.getMessage());
+                System.err.println(e.getMessage());
+            }
         }
     }
-
 }
--- instant-messenger/src/main/java/ru/spbau/mit/SimpleMessageService.java
+++ instant-messenger/src/main/java/ru/spbau/mit/SimpleMessageService.java
@@ -11,29 +11,30 @@ import java.util.concurrent.ConcurrentMap;
 
 public class SimpleMessageService extends MessageServiceImplBase {
     private static final Logger logger = LogManager.getLogger("service");
-    private final ConcurrentMap<String, StreamObserver<Message>> clientObservers = new ConcurrentHashMap<>();
-
-    SimpleMessageService() {
-        super();
-        clientObservers.put("service", new StreamObserver<Message>() {
-            @Override
-            public void onNext(Message value) {
-                logger.info("message received: \n" + value);
-            }
-
-            @Override
-            public void onError(Throwable t) {
-                logger.error(t.getMessage());
-                System.exit(1);
-            }
-
-            @Override
-            public void onCompleted() {
-                logger.info("onCompleted");
-                System.exit(0);
-            }
-        });
-    }
+//    private final StreamObserver<Message> observer;
+////    private final ConcurrentMap<String, StreamObserver<Message>> clientObservers = new ConcurrentHashMap<>();
+//
+//    SimpleMessageService() {
+//        super();
+//        observer = new StreamObserver<Message>() {
+//            @Override
+//            public void onNext(Message value) {
+//                logger.info("message received: \n" + value);
+//            }
+//
+//            @Override
+//            public void onError(Throwable t) {
+//                System.err.println(t.getMessage());
+//                logger.error(t.getMessage());
+////                System.exit(1);
+//            }
+//
+//            @Override
+//            public void onCompleted() {
+//                logger.info("Exiting");
+//            }
+//        };
+//    }
 
     @Override
     public StreamObserver<Message> chat(final StreamObserver<Message> responseObserver) {
@@ -41,27 +42,15 @@ public class SimpleMessageService extends MessageServiceImplBase {
             @Override
             public void onNext(Message request) {
                 String from = request.getSender();
-                if (!clientObservers.containsKey(from)) {
-                    clientObservers.put(from, responseObserver);
-                }
-                StreamObserver<Message> observer = clientObservers.get(from);
-                String receiver = request.getReceiver();
-                if (!clientObservers.containsKey(receiver)) {
-                    String errorMessage = "ERROR: invalid receiver: " + receiver;
-                    observer.onNext(Message.newBuilder()
-                            .setText(errorMessage)
-                            .setSender("service")
-                            .setReceiver(from)
-                            .setTime(currentDateString())
-                            .build());
-                } else {
-                    clientObservers.get(receiver).onNext(request);
-                }
+                String message = from + "wrote: " + request.getText();
+                logger.info("received message: \n" + request);
+                System.out.println(message);
             }
 
             @Override
             public void onError(Throwable t) {
                 logger.error(t.getMessage());
+                System.err.println("ERROR: " + t.getMessage());
             }
 
             @Override
